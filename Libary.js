62d2c2779ccb7ca86a78c8fadf2b7352a2f2f6b5
Taliesin <taliesinstudy@gmail.com>
2024-05-13 16:29:46 +1000

    feat! class examples

---
+++ Libary.js
@@ -0,0 +1,15 @@
+
+
+
+class Libary{
+    constructor(){
+
+        this.books = [];
+        this.audiobooks = [];
+        this.games = [];
+        this.movies = [];
+        this.music =[];
+    }
+}
+
+module.exports = {Libary}
---
+++ app.js
@@ -0,0 +1,18 @@
+// Import Libary class, Book Class, Audiobook class, Games Cclass, Movies Class, Music class
+
+const { Media } = require("./Media");
+
+// Create instances of classes
+
+let lotr = new Media("fellowship of the ring");
+
+console.log(lotr)
+// Lord of the Rings as a instance of Book class
+
+// Do things with those instances
+
+// Create an instance of a Libary
+// add media instances into the libary instance
+
+// Log the data in the libary
+
